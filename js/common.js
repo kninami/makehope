e05b4c759c38ad4f6737bb0afd15f4fd6f9df7bc
ganguri <ganguri@parti.xyz>
2018-02-21 14:26:44 +0900

    오류를 수정합니다

--- js/common.js
+++ js/common.js
@@ -10,18 +10,4 @@ $(document).ready(function(){
       prevEl: '.swiper-button-prev',
     },
   });
-
-  //countup
-  var options = {
-    useEasing: true,
-    useGrouping: true,
-    separator: ',',
-    decimal: '.',
-  };
-  var userCount = new CountUp('user-countup', 0, 59, 0, 1.0, options);
-  if (!userCount.error) {
-    userCount.start();
-  } else {
-    console.error(userCount.error);
-  }
 })
